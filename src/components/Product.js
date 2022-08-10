c3b19665b2d3bd6f2ebf9a5f05e0e219497514e1
MitGanesh <ganeshmittha88@gmail.com>
2022-08-10 23:50:39 +0530

    Shopping list UI comp done

---
+++ src/components/Product.js
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+import { Search, ShoppingCartOutlined, FavoriteBorderOutlined } from '@mui/icons-material'
+
+const Info = styled.div`
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 3;
+    background-color: rgba(0,0,0,0.2);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const Container = styled.div`
+    flex: 1;
+    margin: 5px;
+    min-width: 280px;
+    height: 350px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #f7f7f7;
+    position: relative;
+
+    &:hover ${Info} {
+        opacity: 1;
+    }
+`
+
+const Image = styled.img`
+    height: 75%;
+    z-index: 2;
+`
+
+
+const Icon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 10px;
+    cursor: pointer;
+    transition: all 0.5s ease;
+
+    &:hover{
+        background-color: #e9f5f5;
+        transform: scale(1.1);
+    }
+`
+
+
+const Product = ({ item }) => {
+    return (
+        <Container>
+            <Image src={item.img} />
+            <Info>
+                <Icon>
+                    <ShoppingCartOutlined />
+                </Icon>
+                <Icon>
+                    <Search />
+                </Icon>
+                <Icon>
+                    <FavoriteBorderOutlined />
+                </Icon>
+            </Info>
+        </Container>
+    )
+}
+
+export default Product
--- src/data.js
+++ src/data.js
@@ -39,3 +39,37 @@ export const categories = [
         title: "LIGHT JACKETS",
     },
 ];
+export const products = [
+    {
+        id: 1,
+        img: "https://www.kindpng.com/picc/m/722-7226600_adams-check-shirt-mens-white-dress-shirt-with.png",
+    },
+    {
+        id: 2,
+        img: "https://www.kindpng.com/picc/m/4-41060_mens-fashion-models-png-transparent-png.png",
+    },
+    {
+        id: 3,
+        img: "https://www.kindpng.com/picc/m/788-7887398_gentleman-hd-png-download.png",
+    },
+    {
+        id: 4,
+        img: "https://www.kindpng.com/picc/m/38-386904_transparent-trouser-png-pant-trouser-in-bangladesh-png.png",
+    },
+    {
+        id: 5,
+        img: "https://www.kindpng.com/picc/m/23-239294_boucheron-quatre-perfume-woman-hd-png-download.png",
+    },
+    {
+        id: 6,
+        img: "https://www.kindpng.com/picc/m/283-2839164_professional-cosmetic-product-png-transparent-png.png",
+    },
+    {
+        id: 7,
+        img: "https://www.kindpng.com/picc/m/539-5394315_watch-womens-watch-female-watches-wrist-watch-for.png",
+    },
+    {
+        id: 8,
+        img: "https://www.kindpng.com/picc/m/489-4898921_dont-sweat-it-beauty-the-fitish-class-don.png",
+    },
+];
