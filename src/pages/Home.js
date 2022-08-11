4ba068bd70fd302d176e6739306615c6c6e98ffd
MitGanesh <ganeshmittha88@gmail.com>
2022-08-11 22:06:23 +0530

    Footer comp added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Announcement from '../components/Announcement'
 import Categories from '../components/Categories'
+import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import NewsLetter from '../components/NewsLetter'
 import Products from '../components/Products'
@@ -9,12 +10,13 @@ import Slider from '../components/Slider'
 const Home = () => {
   return (
     <div>
-        <Announcement/>
-        <Navbar/>
-        <Slider/>
-        <Categories/>
-        <Products/>
-        <NewsLetter/>
+      <Announcement />
+      <Navbar />
+      <Slider />
+      <Categories />
+      <Products />
+      <NewsLetter />
+      <Footer />
     </div>
   )
 }
