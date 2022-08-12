b53693f569299871c8bb6e7d47a4a76cec18ad21
MitGanesh <ganeshmittha88@gmail.com>
2022-08-12 20:28:57 +0530

    Navbar responsive has fixed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
-import Cart from './pages/Cart';
+// import Cart from './pages/Cart';
 // import Login from './pages/Login';
 // import Product from './pages/Product';
 // import Register from './pages/Register';
-// import Home from './pages/Home';
+import Home from './pages/Home';
 // import ProductList from './pages/ProductList';
 
 function App() {
   return (
     <div>
-      {/* <Home/> */}
+      <Home/>
       {/* <ProductList/> */}
       {/* <Product/> */}
       {/* <Register/> */}
       {/* <Login/> */}
-      <Cart/>
+      {/* <Cart/> */}
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,14 @@ import { Search, ShoppingCartOutlined } from '@mui/icons-material'
 import { Badge } from '@mui/material'
 import React from 'react'
 import styled from 'styled-components'
+import {mobile} from '../reponsive'
 
 const Container = styled.div`
     height: 60px;
+
+    ${mobile({
+        height: '50px',
+    })}
 `
 
 const Wrapper = styled.div`
@@ -12,6 +17,10 @@ const Wrapper = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
+
+    ${mobile({
+        padding: '10px 0px',
+    })}
 `
 
 const Left = styled.div`
@@ -26,21 +35,37 @@ const SearchContainer = styled.div`
     align-items: center;
     margin-left: 25px;
     padding: 5px;
+
+    ${mobile({
+        marginLeft: '10px',
+    })}
 `
 
 const Input = styled.input`
     border: none;
+
+    ${mobile({
+        width: '50px',
+    })}
 `
 
 const Logo = styled.h1`
     font-weight: bold;
     display: flex;
     justify-content: center;
+
+    ${mobile({
+        fontSize: '24px',
+    })}
 `
 
 const Language = styled.span`
     font-size: 14px;
     cursor: pointer;
+
+    ${mobile({
+        display: 'none',
+    })}
 `
 
 const Center = styled.div`
@@ -52,12 +77,22 @@ const Right = styled.div`
     display: flex;
     align-items: center;
     justify-content: flex-end;
+
+    ${mobile({
+        justifyContent: 'center',
+        flex: 2,
+    })}
 `
 
 const MenuItem = styled.div`
     font-size: 14px;
     cursor: pointer;
     margin-left: 25px;
+
+    ${mobile({
+        fontSize: '12px',
+        marginLeft: '10px',
+    })}
 `
 
 
@@ -70,7 +105,7 @@ const Navbar = () => {
                         EN
                     </Language>
                     <SearchContainer>
-                        <Input />
+                        <Input placeholder='Search'/>
                         <Search style={{color:'grey',fontSize:18}} />
                     </SearchContainer>
                 </Left>
